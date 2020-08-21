f00bd9e5b8200bb00112bf5404ed41a42cd64ce5
yuki-hirano <yukiy99999@yahoo.co.jp>
2020-08-22 08:05:46 +0900

    最新動作確認状態

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -77,14 +77,7 @@ public class CreateServlet extends HttpServlet {
                 // indexのページにリダイレクト
                 response.sendRedirect(request.getContextPath() + "/index");
             }
-            
-            em.getTransaction().begin();
-            em.persist(m);
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "登録が完了しました。");
-            em.close();
 
-            response.sendRedirect(request.getContextPath() + "/index");
         }
     }
 
